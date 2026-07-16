8687b971f8ec20f37d270bec074d99cfafaca951
Astraalto <cyberindygos@gmail.com>
2026-07-16 14:10:13 +0300

    1.05 - Questions updated

--- src/questions.py
+++ src/questions.py
@@ -59,4 +59,94 @@ questions = [
         },
         "answer": "A"
     },
+    {
+        "question": "What is the national instrumanent of Finland?",
+        "options": {
+            "A": "Violin",
+            "B": "Electric Guitar",
+            "C": "Kantele",
+            "D": "Bagpipes"
+        },
+        "answer": "C"
+    },
+     {
+        "question": "What is the national animal of Finland?",
+        "options": {
+            "A": "Gray wolf",
+            "B": "Eurasian Lynx",
+            "C": "Finnish forest reindeer",
+            "D": "Brown bear"
+        },
+        "answer": "D"
+    },
+    {
+        "question": "What is the national bird of Finland?",
+        "options": {
+            "A": "Whooper swan",
+            "B": "Snow owl",
+            "C": "White stork",
+            "D": "Taiga bean goose"
+        },
+        "answer": "A"
+    },
+    {
+        "question": "What is the national fish of Finland?",
+        "options": {
+            "A": "Brown trout",
+            "B": "Whitefish",
+            "C": "Northern Pike",
+            "D": "European perch"
+        },
+        "answer": "D"
+    },
+    {
+        "question": "What is the national flower of Finland?",
+        "options": {
+            "A": "Lily of the valley",
+            "B": "Common Dog-violet",
+            "C": "Tufted Vetch",
+            "D": "Bluebell"
+        },
+        "answer": "A"
+    },
+    {
+        "question": "Who served as the only female president in Finland?",
+        "options": {
+            "A": "Sanna Marin",
+            "B": "Tarja Turunen",
+            "C": "Li Andersson",
+            "D": "Tarja Halonen"
+        },
+        "answer": "D"
+    },
+    {
+        "question": "Who won Nobel Prize in Literature in 1939?",
+        "options": {
+            "A": "Antti Tuomainen",
+            "B": "Mika Waltari",
+            "C": "Frans Eemil Sillanpää",
+            "D": "Tove Jansson"
+        },
+        "answer": "C"
+    },
+    {
+        "question": "What is the longest Finnish river?",
+        "options": {
+            "A": "Ounasjoki",
+            "B": "Kemijoki",
+            "C": "Tornionjoki",
+            "D": "Lihajoki"
+        },
+        "answer": "B"
+    },
+    {
+        "question": "Finland was the first country in Europe where women got the right to vote. In what year did this happen?",
+        "options": {
+            "A": "1906",
+            "B": "1909",
+            "C": "1917",
+            "D": "1918"
+        },
+        "answer": "A"
+    }
 ]
